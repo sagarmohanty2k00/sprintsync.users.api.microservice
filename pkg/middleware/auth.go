e4118e014c2de3b7e09383976527089fcc28a7e7
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2024-06-26 09:18:59 +0530

    add auth mmiddleware for authorized routes

---
+++ pkg/middleware/auth.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"sprintsync.com/users/pkg/auth"
+)
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.Split(authHeader, " ")[1]
+		_, err := auth.VerifyToken(tokenStr)
+		if err != nil {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
