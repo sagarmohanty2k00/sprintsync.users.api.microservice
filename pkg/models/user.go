08bf5ce72252af60c6fbab1bbbcd8fb770988d9f
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2024-06-26 08:48:07 +0530

    create user model

---
+++ pkg/models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	FullName     string    `json:"full_name"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
